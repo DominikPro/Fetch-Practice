bd6a64e97210c0525cababc247d743221e62a07b
Dominik Proficz <dominik.proficz@gmail.com>
2019-08-20 20:23:51 +0200

    Added style sheet for UserList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "./UsersList.css";
 
 const UserList = props => {
   const users = props.users.map(user => (
     <div className="user" key={user.login.uuid}>
+      <img src={user.picture.large} alt={user.name.last} />
       <h2>{`${user.name.title} ${user.name.last}`}</h2>
       <p>{user.email}</p>
     </div>
